ef56cf665d1299df52391d743ae6eb08df96816e
Elias Moran <yoexereliasmoranurcia@gmail.com>
2025-08-23 18:13:35 -0500

    01-23082025-Configuracion inicial del proyecto de Spring boot

---
+++ src/main/java/com/confeccionesdym/confecciones_dym/ConfeccionesDymApplication.java
@@ -0,0 +1,13 @@
+package com.confeccionesdym.confecciones_dym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfeccionesDymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfeccionesDymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/confeccionesdym/confecciones_dym/ConfeccionesDymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confeccionesdym.confecciones_dym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeccionesDymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
