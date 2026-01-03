7444b6ae5d8bf71d66cff3e77ea61c5d0b1b9db2
EliasMoran <yoexereliasmoranurcia@gmail.com>
2026-01-03 13:12:24 -0500

    Impelmentando Auditoría en el módulo Cliente

--- src/main/java/com/confeccionesdym/confecciones_dym/ConfeccionesDymApplication.java
+++ src/main/java/com/confeccionesdym/confecciones_dym/ConfeccionesDymApplication.java
@@ -2,8 +2,10 @@ package com.confeccionesdym.confecciones_dym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ConfeccionesDymApplication {
 
 	public static void main(String[] args) {
