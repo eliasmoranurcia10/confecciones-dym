35f66e7027945a51f9475aa2ea1a71d2eccf946b
Yoexer Elias Moran Urcia <yoexereliasmoranurcia@gmail.com>
2026-04-12 20:21:52 -0500

    Prioriza la seguridad solicitando la autenticación

--- src/main/java/com/confeccionesdym/confecciones_dym/config/SecurityConfig.java
+++ src/main/java/com/confeccionesdym/confecciones_dym/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.confeccionesdym.confecciones_dym.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,8 +13,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests( auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
